36a6071b43f8ef8fcf96247ae21dd53fca9ea5a2
Kafstinolli <agomez47@uan.edu.co>
2025-04-02 22:35:56 -0500

    Pantalla de Historial finalizada: avatar, nombre, fondo, tarjetas con sombra y diseño completo según mockup

--- app/src/main/java/com/example/petcare/ui/theme/screens/HealthBlogPage.kt
+++ app/src/main/java/com/example/petcare/ui/theme/screens/HealthBlogPage.kt
@@ -1,28 +1,35 @@
 package com.example.petcare.ui.theme.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.petcare.R
+
 //import com.example.petcare.ui.theme.Screen
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun HealthBlogPage() {
     Box(modifier = Modifier.fillMaxSize()) {
@@ -52,7 +59,7 @@ fun HealthBlogPage() {
                 Image(
                     painter = painterResource(id = R.drawable.logo_home),
                     contentDescription = "Logo Izquierdo",
-                    modifier = Modifier.size(48.dp)
+                    modifier = Modifier.size(44.dp)
                 )
 
                 // Título
@@ -69,7 +76,7 @@ fun HealthBlogPage() {
                 Image(
                     painter = painterResource(id = R.drawable.logo),
                     contentDescription = "Logo Derecho",
-                    modifier = Modifier.size(58.dp)
+                    modifier = Modifier.size(54.dp)
                 )
             }
 
