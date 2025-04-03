47bf817d019468981639101dfe102ee59c2f206f
Mateo Villamizar <matiusvil234@gmail.com>
2025-04-02 20:27:26 -0500

    BlogPostScreen UI Completed
    BlogPostScreen UI completed and removed unused composable from Navigation

--- app/src/main/java/com/example/petcare/ui/theme/screens/BlogPostScreen.kt
+++ app/src/main/java/com/example/petcare/ui/theme/screens/BlogPostScreen.kt
@@ -1,88 +1,123 @@
 package com.example.petcare.ui.theme.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.petcare.R
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class) // Anotación para usar Scaffold con ExperimentalMaterial3Api
+@Preview(showBackground = true)
 @Composable
-fun BlogPostScren(navController: NavController) {
+fun BlogPostScreen() {
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
-                    Text(
-                        text = "Blog Salud",
-                        textAlign = TextAlign.Center,
-                        color = androidx.compose.ui.graphics.Color(0xFF8e71ea),
-                        fontFamily = FontFamily.Default,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 50.sp
-                    )
-                },
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.blog_icon),
+                            contentDescription = "Blog logo",
+                            modifier = Modifier
+                                .size(48.dp)
+                        )
+
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        Text(
+                            text = "Blog de Salud",
+                            textAlign = TextAlign.Center,
+                            color = Color(0xFF7555a7),
+                            fontFamily = FontFamily.Default,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 40.sp,
+                            modifier = Modifier.wrapContentWidth()
+                        )
+
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        Image(
+                            painter = painterResource(id = R.drawable.logo),
+                            contentDescription = "Petcare logo",
+                            modifier = Modifier
+                                .size(48.dp)
+                        )
+                    }
+                }
             )
         }
     ) { innerPadding ->
+
         LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
         ) {
             item {
+                BlogHorizontalDivider()
+            }
+
+            item {
                 Image(
-                    painter = painterResource(id = R.drawable.logo),
-                    contentDescription = "Blog Image",
+                    painter = painterResource(id = R.drawable.health_blog_image),
+                    contentDescription = "Blog image",
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 8.dp, end = 8.dp)
+                        .size(500.dp)
+                        .padding(top = 16.dp, bottom = 16.dp)
                 )
-                Spacer(modifier = Modifier.height(16.dp))
+            }
+
+            item {
+                BlogHorizontalDivider()
             }
 
             item {
                 Text(
-                    text ="Texto del artículo completo",
-                    modifier = Modifier.padding(16.dp)
+                    text = stringResource(id = R.string.blog_text),
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Justify,
+                    color = Color(0xFF636363),
+                    modifier = Modifier.padding(start = 24.dp, end = 24.dp, top = 16.dp, bottom = 16.dp)
                 )
-
             }
-
         }
     }
 }
 
-
-
-
-
-
-
-
-
-
-@Preview(showBackground = true)
 @Composable
-fun BlogPostScrenPreview() {
-    val navController = rememberNavController()
-    BlogPostScren(navController = navController)
+fun BlogHorizontalDivider() {
+    HorizontalDivider(
+        thickness = 4.dp,
+        color = Color(0xFF7555a7),
+        modifier = Modifier.fillMaxWidth(0.9f) //solo el 80% del ancho de la pantalla
+    )
 }
