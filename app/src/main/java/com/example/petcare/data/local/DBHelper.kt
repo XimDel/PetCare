b116079fb61d02bca546f8573d3cd06771351c37
Santiago Rojas <santirojastr12@gmail.com>
2025-05-05 21:57:40 -0500

    Create database structure for users and breeds
    The changes introduce the initial database structure for the application, including the creation of the `veterinaria.db` database.
    The `DBHelper` class now handles the creation of tables: `tipo_usuario`, `usuario`, and `raza`.
    The `DBContract` object defines the SQL queries to create these tables.

---
+++ app/src/main/java/com/example/petcare/data/local/DBHelper.kt
@@ -0,0 +1,35 @@
+package com.example.petcare.data.local
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.example.petcare.data.local.DBContract.SQL_CREATE_RAZA
+import com.example.petcare.data.local.DBContract.SQL_CREATE_TIPO_USUARIO
+import com.example.petcare.data.local.DBContract.SQL_CREATE_USUARIO
+
+class DBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(SQL_CREATE_TIPO_USUARIO)
+        db.execSQL(SQL_CREATE_USUARIO)
+//        db.execSQL(SQL_CREATE_TIPO_MASCOTA)
+        db.execSQL(SQL_CREATE_RAZA)
+//        db.execSQL(SQL_CREATE_MASCOTA)
+//        db.execSQL(SQL_CREATE_IMAGEN_MASCOTA)
+//        db.execSQL(SQL_CREATE_TIPO_CITA)
+//        db.execSQL(SQL_CREATE_CITA)
+//        db.execSQL(SQL_CREATE_VACUNA)
+//        db.execSQL(SQL_CREATE_MEDICAMENTO)
+//        db.execSQL(SQL_CREATE_TRATAMIENTO)
+//        db.execSQL(SQL_CREATE_CONDICION_MEDICA)
+//        db.execSQL(SQL_CREATE_BLOG)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+    }
+
+    companion object {
+        const val DATABASE_NAME = "veterinaria.db"
+        const val DATABASE_VERSION = 1
+    }
+}
