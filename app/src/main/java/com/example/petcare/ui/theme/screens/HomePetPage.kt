86a009e637a2235e593b77e25eee506474e901be
Ximena Delgado <xim.delgado@outlook.com>
2025-05-23 10:50:09 -0500

    Actualizacion de opciones

--- app/src/main/java/com/example/petcare/ui/theme/screens/HomePetPage.kt
+++ app/src/main/java/com/example/petcare/ui/theme/screens/HomePetPage.kt
@@ -61,7 +61,7 @@ fun HomeScreen(navController: NavHostController, idMascota: Int) {
         mascota = dao.obtenerMascotas().find { it.idMascota == idMascota }
     }
 
-    val petName = mascota?.nombre ?: "Cargando..."
+    val petName = mascota?.nombre ?: "Tu mascota"
 
     Box(modifier = Modifier.fillMaxSize()) {
         // Imagen de fondo
